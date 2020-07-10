a79807dc3b924282e8ba7e2225f7a7aa74da70d9
Bruce Howard <brucehoward@twc.com>
2020-07-10 10:12:10 -0400

    Through the Sorting Queries lesson

--- src/ComicBookGalleryModel/ComicBookGalleryModel/Program.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ComicBookGalleryModel.Models;
 using System;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 
 namespace ComicBookGalleryModel
@@ -13,21 +14,65 @@ namespace ComicBookGalleryModel
             using (var context = new Context())
             {
 
-                var comicBooks = context.ComicBooks
+                context.Database.Log = (message) => Debug.WriteLine(message);
+
+                //var comicBooks = context.ComicBooks
+                //    .Include(cb => cb.Series)
+                //    .OrderByDescending(cb => cb.IssueNumber)
+                //    .ThenBy(cb => cb.PublishedOn)
+                //    //.Where(cb => cb.IssueNumber == 1 || cb.Series.Title == "The Amazing Spider-Man")
+                //    .Where(cb => cb.Series.Title.Contains("man"))
+                //    .ToList();
+
+                var comicBooksQuery = context.ComicBooks
                     .Include(cb => cb.Series)
-                    .Include(cb => cb.Artists.Select(a=> a.Artist))
-                    .Include(cb => cb.Artists.Select(a => a.Role))
-                    .ToList();
+                    .OrderByDescending(cb => cb.IssueNumber);
+
+                var comicBooks = comicBooksQuery.ToList();
+
+                var comicBooks2 = comicBooksQuery.Where(cb => cb.AverageRating < 7).ToList();
 
                 foreach (var comicBook in comicBooks)
                 {
-                    var artistRoleNames = comicBook.Artists
-                        .Select(a => $"{a.Artist.Name} - {a.Role.Name}").ToList();
-                    var artistRolesDisplayText = string.Join(", ", artistRoleNames);
                     Console.WriteLine(comicBook.DisplayText);
-                    Console.WriteLine(artistRolesDisplayText);
                 }
 
+
+
+                //var comicBooksQuery = from cb in context.ComicBooks select cb;
+                //var comicBooks = comicBooksQuery.ToList();
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("# of comic books: {0}", comicBooks.Count);
+
+
+                foreach (var comicBook in comicBooks2)
+                {
+                    Console.WriteLine(comicBook.DisplayText);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("# of comic books: {0}", comicBooks2.Count);
+
+
+                #region preListQuery
+                //var comicBooks = context.ComicBooks
+                //    .Include(cb => cb.Series)
+                //    .Include(cb => cb.Artists.Select(a=> a.Artist))
+                //    .Include(cb => cb.Artists.Select(a => a.Role))
+                //    .ToList();
+
+                //foreach (var comicBook in comicBooks)
+                //{
+                //    var artistRoleNames = comicBook.Artists
+                //        .Select(a => $"{a.Artist.Name} - {a.Role.Name}").ToList();
+                //    var artistRolesDisplayText = string.Join(", ", artistRoleNames);
+                //    Console.WriteLine(comicBook.DisplayText);
+                //    Console.WriteLine(artistRolesDisplayText);
+                //}
+
+                #endregion
                 Console.ReadLine();
             }
 
